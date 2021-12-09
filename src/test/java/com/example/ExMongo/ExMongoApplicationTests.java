c4220a64b00c93011a161fb397828457fe90c97f
Matheus <matheusprgon@gmail.com>
2021-12-09 15:33:45 -0300

    First Commit

---
+++ src/test/java/com/example/ExMongo/ExMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
